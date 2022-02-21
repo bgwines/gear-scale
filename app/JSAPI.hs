9928d018af76c608073042a3c2cb194120e6b511
Brett Wines <bwines@quip.com>
2022-02-20 21:21:41 -0800

    refactor TSDef to separate executable

--- app/JSAPI.hs
+++
@@ -1,9 +0,0 @@
-module JSAPI
-  ( writeJSFiles
-  ) where
-
-import qualified ServerAPI
-import Servant.JS ( writeJSForAPI, vanillaJS )
-
-writeJSFiles :: IO ()
-writeJSFiles = writeJSForAPI ServerAPI.limitedApi vanillaJS "static/api.js"
